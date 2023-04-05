e80a2b42519fc2a2571d5e5fd451750a4e69e480
Luttarios-069 <117871011+luizcarlosom@users.noreply.github.com>
2023-04-05 16:04:52 -0300

    Criação do programa 1, 2 e 3. Sendo o 3 com um problema que é necessário ser resolvido.

---
+++ programa02/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void){
+
+    time_t epoch = time(NULL);//função para pegar os segundos desde 1 de janeiro de 1970 ou epoch
+    FILE *pont_arq;//variavel ponteiro para criação do txt
+    struct tm *tm = localtime(&epoch);//importando bibilioteca para pegar hora, ano...
+
+    int contador = 1; //contador
+
+    //hora
+    int horas = tm -> tm_hour;
+    int minutos = tm -> tm_min;
+    int segundos = tm -> tm_sec;
+
+    //data
+    int mes = tm -> tm_mon + 1;
+    int ano = tm -> tm_year + 1900;
+    int dia = tm -> tm_mday;
+
+    char concatenacao[100];//variavel onde concateno todos os dados do arquivo
+    
+    //Abrindo ou criando o arquivo
+    pont_arq = fopen("REGISTRO2.txt", "w");
+    printf("Arquivo criado  ou aberto!");
+
+    //verificando se o arquivo foi aberto
+    if(pont_arq == NULL){
+
+        printf("ERR0!");
+        return 1;
+    }else{
+        
+        while(contador <= 10000){
+            sprintf(concatenacao, "2 programa2 - Thread%d - Data: %02d/%02d/%d Horário: %02d:%02d:%02d ", contador, dia, mes, ano, horas, minutos, segundos);//função que vai juntar todos os dados e colocar em uma só variavel "concatenacao"
+            fprintf(pont_arq, "%s\n", concatenacao);//função que escreve dentro do arquivo "pont_arq"
+
+            contador++;//incrementando
+        }
+        
+        fclose(pont_arq);//fechando o arquivo
+        printf("Arquivo fechado!");
+    }
+
+    return 0;
+}
---
+++ programa03/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <time.h>
+
+int programa1(void){
+
+    time_t epoch = time(NULL);//função para pegar os segundos desde 1 de janeiro de 1970 ou epoch
+    FILE *pont_arq;//variavel ponteiro para criação do txt
+    struct tm *tm = localtime(&epoch);//importando bibilioteca para pegar hora, ano...
+
+    int contador = 1; //contador
+
+    //hora
+    int horas = tm -> tm_hour;
+    int minutos = tm -> tm_min;
+    int segundos = tm -> tm_sec;
+
+    //data
+    int mes = tm -> tm_mon + 1;
+    int ano = tm -> tm_year + 1900;
+    int dia = tm -> tm_mday;
+
+    char concatenacao[100];//variavel onde concateno todos os dados do arquivo
+    
+    //Abrindo ou criando o arquivo
+    pont_arq = fopen("REGISTRO_GERAL.txt", "w");
+    printf("Arquivo criado  ou aberto!");
+        
+    while(contador <= 5000){
+        sprintf(concatenacao, "1 programa1 - Thread%d - Data: %02d/%02d/%d Horário: %02d:%02d:%02d ", contador, dia, mes, ano, horas, minutos, segundos);//função que vai juntar todos os dados e colocar em uma só variavel "concatenacao"
+        fprintf(pont_arq, "%s\n", concatenacao);//função que escreve dentro do arquivo "pont_arq"
+
+        contador++;//incrementando
+    }
+    
+    fclose(pont_arq);//fechando o arquivo
+    printf("Arquivo fechado!");
+    
+}
+
+int programa2(void){
+
+    time_t epoch = time(NULL);//função para pegar os segundos desde 1 de janeiro de 1970 ou epoch
+    FILE *pont_arq;//variavel ponteiro para criação do txt
+    struct tm *tm = localtime(&epoch);//importando bibilioteca para pegar hora, ano...
+
+    int contador = 1; //contador
+
+    //hora
+    int horas = tm -> tm_hour;
+    int minutos = tm -> tm_min;
+    int segundos = tm -> tm_sec;
+
+    //data
+    int mes = tm -> tm_mon + 1;
+    int ano = tm -> tm_year + 1900;
+    int dia = tm -> tm_mday;
+
+    char concatenacao[100];//variavel onde concateno todos os dados do arquivo
+    
+    //Abrindo ou criando o arquivo
+    pont_arq = fopen("REGISTRO_GERAL.txt", "w");
+    printf("Arquivo criado  ou aberto!");
+    
+    while(contador <= 10000){
+        sprintf(concatenacao, "2 programa2 - Thread%d - Data: %02d/%02d/%d Horário: %02d:%02d:%02d ", contador, dia, mes, ano, horas, minutos, segundos);//função que vai juntar todos os dados e colocar em uma só variavel "concatenacao"
+        fprintf(pont_arq, "%s\n", concatenacao);//função que escreve dentro do arquivo "pont_arq"
+
+        contador++;//incrementando
+    }
+    
+    fclose(pont_arq);//fechando o arquivo
+    printf("Arquivo fechado!");
+
+}
+
+int main(void){
+    programa1();
+    programa2();
+}
+
+
